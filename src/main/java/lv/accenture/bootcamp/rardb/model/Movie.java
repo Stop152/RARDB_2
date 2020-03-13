de17fd3e34fb0ee10cc67f1c3256e38de0db5cd8
andkristine <kristine.andrejeva33@gmail.com>
2020-03-13 17:00:20 +0200

    Movie reader in process

---
+++ src/main/java/lv/accenture/bootcamp/rardb/model/Movie.java
@@ -0,0 +1,11 @@
+package lv.accenture.bootcamp.rardb.model;
+
+public class Movie {
+
+	String title; //vai variable names no json nav case sensitive?
+	Integer year;
+	
+	
+	
+	
+}
---
+++ src/main/java/lv/accenture/bootcamp/rardb/movieAPI/MovieAPIService.java
@@ -0,0 +1,70 @@
+package lv.accenture.bootcamp.rardb.movieAPI;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class MovieAPIService {
+
+//	@Value("@{api.omdb}") 
+//	private String requestUrl;
+	private String requestUrl = "http://www.omdbapi.com/?t=Fight%20Club&apikey=f5a0f60f";
+	
+	@Value("@{api.omdb.key}")
+	private String apiKey;
+	
+	public void getMovie(String requestedMovieTitle) {
+		
+		try {
+//			URL url = new URL(requestUrl + "?t=" + requestedMovieTitle + apiKey);
+			URL url = new URL(requestUrl);
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(3000);
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            
+            String jsonResponse = sb.toString();
+            System.out.println(jsonResponse);
+			
+			
+			
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			
+			throw new RuntimeException(e);
+		}
+		
+		
+		
+	}
+	
+	public static void main (String[] args) {
+		
+		
+		MovieAPIService service = new MovieAPIService();
+		service.getMovie("Fight Club");
+	}
+	
+	
+	
+
+}
