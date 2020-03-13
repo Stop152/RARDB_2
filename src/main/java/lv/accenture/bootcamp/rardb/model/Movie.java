ee00c1bf3bc71906952f2aa402cceffcc9b72401
andkristine <kristine.andrejeva33@gmail.com>
2020-03-13 20:06:51 +0200

    Movie, movie API, application properties

--- src/main/java/lv/accenture/bootcamp/rardb/model/Movie.java
+++ src/main/java/lv/accenture/bootcamp/rardb/model/Movie.java
@@ -2,7 +2,7 @@ package lv.accenture.bootcamp.rardb.model;
 
 public class Movie {
 
-	String title; //vai variable names no json nav case sensitive?
+	String Title; //vai variable names no json nav case sensitive?
 	Integer year;
 	
 	
--- src/main/java/lv/accenture/bootcamp/rardb/movieAPI/MovieAPIService.java
+++ src/main/java/lv/accenture/bootcamp/rardb/movieAPI/MovieAPIService.java
@@ -5,66 +5,55 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class MovieAPIService {
 
-//	@Value("@{api.omdb}") 
-//	private String requestUrl;
-	private String requestUrl = "http://www.omdbapi.com/?t=Fight%20Club&apikey=f5a0f60f";
-	
-	@Value("@{api.omdb.key}")
+	@Value("${api.omdb.base}")
+	private String requestUrl;
+//	private String requestUrl = "http://www.omdbapi.com/?t=Fight%20Club&apikey=f5a0f60f";
+
+	@Value("${api.omdb.key}")
 	private String apiKey;
-	
+
 	public void getMovie(String requestedMovieTitle) {
-		
+
 		try {
-//			URL url = new URL(requestUrl + "?t=" + requestedMovieTitle + apiKey);
-			URL url = new URL(requestUrl);
+			URL url = new URL(requestUrl + "?t=" + requestedMovieTitle + "&apikey=" + apiKey);
+			// URL url= new URL(requestUrl);
 			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 			urlConnection.setRequestMethod("GET");
-            urlConnection.setReadTimeout(3000);
-            urlConnection.connect();
+			urlConnection.setReadTimeout(3000);
+			urlConnection.connect();
 
-            InputStream inputStream = urlConnection.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            
-            StringBuilder sb = new StringBuilder();
+			InputStream inputStream = urlConnection.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            
-            String jsonResponse = sb.toString();
-            System.out.println(jsonResponse);
-			
-			
-			
-			
-			
-			
-			
-			
+			StringBuilder sb = new StringBuilder();
+
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+
+			String jsonResponse = sb.toString();
+			System.out.println(jsonResponse);
+
 		} catch (Exception e) {
-			
+
 			throw new RuntimeException(e);
 		}
-		
-		
-		
+
 	}
-	
-	public static void main (String[] args) {
-		
-		
+
+	public static void main(String[] args) {
+
 		MovieAPIService service = new MovieAPIService();
 		service.getMovie("Fight Club");
 	}
-	
-	
-	
 
 }
