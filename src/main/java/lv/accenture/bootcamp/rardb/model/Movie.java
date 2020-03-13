91b694f2ed1a2e21cef4d4f0f4c4d7b954446908
andkristine <kristine.andrejeva33@gmail.com>
2020-03-13 20:25:03 +0200

    Movie klase

--- src/main/java/lv/accenture/bootcamp/rardb/model/Movie.java
+++ src/main/java/lv/accenture/bootcamp/rardb/model/Movie.java
@@ -3,7 +3,36 @@ package lv.accenture.bootcamp.rardb.model;
 public class Movie {
 
 	String Title; //vai variable names no json nav case sensitive?
-	Integer year;
+	Integer Year;
+	String imdbID;
+	String Poster;
+	
+	
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public Integer getYear() {
+		return Year;
+	}
+	public void setYear(Integer year) {
+		Year = year;
+	}
+	public String getImdbID() {
+		return imdbID;
+	}
+	public void setImdbID(String imdbID) {
+		this.imdbID = imdbID;
+	}
+	public String getPoster() {
+		return Poster;
+	}
+	public void setPoster(String poster) {
+		Poster = poster;
+	}
+	
 	
 	
 	
