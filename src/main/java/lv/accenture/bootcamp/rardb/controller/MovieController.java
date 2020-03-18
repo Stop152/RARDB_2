53bcf9eeab798efbab0af65c69a3cc02732080ff
andkristine <kristine.andrejeva33@gmail.com>
2020-03-18 21:37:26 +0200

    Clean updates

--- src/main/java/lv/accenture/bootcamp/rardb/controller/MovieController.java
+++ src/main/java/lv/accenture/bootcamp/rardb/controller/MovieController.java
@@ -1,13 +1,10 @@
 package lv.accenture.bootcamp.rardb.controller;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
-=======
 import java.util.Optional;
 
 import javax.validation.Valid;
->>>>>>> aa066ca99952a73783153be83fbb005d0075c463
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,8 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import lv.accenture.bootcamp.rardb.model.*;
 import lv.accenture.bootcamp.rardb.movieAPI.*;
-import lv.accenture.bootcamp.webdemo.model.Cat;
-
 
 @Controller
 public class MovieController {
@@ -40,7 +35,6 @@ public class MovieController {
 		movieAPIService.getMovieById("tt0096895");
 		return "movies-index";
 	}
-<<<<<<< HEAD
 
 	@GetMapping("/search_results")
 	public String searchIndex(Model model) {
@@ -49,40 +43,24 @@ public class MovieController {
 		movieSearch = movieAPIService.searchMoviePhrase("Batman");
 		model.addAttribute("movieSearch", movieSearch);
 		model.addAttribute("movieCandidates", movieSearch.getSearch());
-		
+
 		return "search-index";
 	}
 
-=======
-	@GetMapping("/search_results")
-	public String searchIndex(Model model) {
-		MovieSearch movieSearch = new MovieSearch();
-		movieSearch = movieAPIService.searchMoviePhrase("Batman");
-		model.addAttribute("movieSearch", movieSearch);
-		MoviePreview moviePreview = new MoviePreview();
-		model.addAttribute("moviePreview", moviePreview);
-		return "search-index";
+	@PostMapping("/movie/add-comment")
+	public String addComment(@Valid Movie commentToAdd, @Valid Movie rankToAdd, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "add-comment";
+		}
+		movieRepository.save(commentToAdd);
+		movieRepository.save(rankToAdd);
+		return "redirect:/movie";
 	}
 
-@PostMapping("/movie/add-comment")
-public String addComment(@Valid Movie commentToAdd, @Valid Movie rankToAdd, BindingResult bindingResult) {
-  if(bindingResult.hasErrors()) {
-	  return "add-comment";
-  }
-movieRepository.save(commentToAdd);
-movieRepository.save(rankToAdd);
-  return "redirect:/movie";
+	@GetMapping("/movie/delete-comment/{imdbID}")
+	public String deleteAllComment(@PathVariable Long imdbID) {
+		movieRepository.deleteComment(imdbID);
+		movieRepository.deleteRank(imdbID);
+		return "redirect:/movie";
+	}
 }
-
-@GetMapping("/movie/delete-comment/{imdbID}")
-public String deleteAllComment(@PathVariable Long imdbID) {
-  movieRepository.deleteComment(imdbID);
-  movieRepository.deleteRank(imdbID);
-  return "redirect:/movie";
-}
->>>>>>> aa066ca99952a73783153be83fbb005d0075c463
-}
-
-
-
-
